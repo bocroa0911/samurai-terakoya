d79f59f5843bc51672b440bf621dea5bd3a428a0
bocroa0911 <146640088+bocroa0911@users.noreply.github.com>
2023-11-09 16:37:25 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,15 @@
 let num = Math.floor(Math.random() * 100);
 
-if (num % 3 === 0) {
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
+}
+
+else if (num % 3 === 0) {
     console.log('３の倍数です');
 }
 
-if (num % 5 === 0) {
+else if (num % 5 === 0) {
     console.log('5の倍数です');
-}
-
-if (num % 3 === 0 && num % 5 === 0) {
-    console.log('3と5の倍数です');
 }
 
 else {
